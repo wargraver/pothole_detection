9a2ac80530115d382dd5ae22b86f3e4cc496a6fb
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-12-26 18:04:22 +0530

    added login for official side

--- Routes/official_auth.js
+++ Routes/official_auth.js
@@ -54,3 +54,61 @@ route.post('/signup', async (req, res) => {
 		return;
 	}
 });
+
+
+// login route
+route.post('/login', async (req, res) => {
+	const official_object = req.body.official_object;
+	try {
+		const { employee_id, password } = req.body;
+		//checking if the field is empty or not
+		if (validator.isEmpty(employee_id) || validator.isEmpty(password)) {
+			official_object.error = 'Please enter all fields';
+			res.status(200).send(JSON.stringify(official_object));
+			return;
+		}
+
+		// checking if the id exists in the db or not
+		const found = await official.findOne({ employee_id });
+		if (found == null) {
+			official_object.error = 'Invalid Credentials';
+			res.status(200).send(JSON.stringify(official_object));
+			return;
+		}
+
+		//checking if the password is correct
+		const hashPass = found.password;
+		const equal = await bcrypt.compare(password, hashPass);
+		console.log(equal);
+		if (equal === false) {
+			official_object.error = 'Invalid Credentials';
+			res.status(200).send(JSON.stringify(official_object));
+			return;
+		}
+
+		// generating token using jwt
+		const gen_token = jwt.sign(employee_id, process.env.auth_official_key);
+
+		// checking if the token already exists in db
+		const val = await token_official.findOne({ value: gen_token });
+		if (val != null) {
+			await token_official.deleteOne({ value: gen_token });
+		}
+		const save_token = new token_official({
+			value: gen_token,
+			owner: found.id,
+		});
+		const vari = await save_token.save();
+		official_object.msg = 'Logged in successfully';
+		official_object.token = gen_token;
+		official_object.employee_id = found.employee_id;
+		official_object.name = found.name;
+		official_object.issues_solved = found.issues_solved;
+		res.status(200).send(JSON.stringify(official_object));
+	} catch (err) {
+		console.log(err);
+		official_object.error = 'Something went wrong while logging in';
+		res.status(500).send(JSON.stringify(official_object));
+	}
+});
+
