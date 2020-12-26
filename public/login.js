39773baeafbf400105b527a30ed8b1bc44ac4161
Utkarsh <utkarshkrsingh2091@gmail.com>
2020-12-26 22:04:37 +0530

    frontend

---
+++ public/login.js
@@ -0,0 +1,28 @@
+function login(employee_id,password,done){
+    $.post('https://bug-slayers.herokuapp.com/official/login',{
+        employee_id:employee_id,
+        password:password
+    },function(data){
+        done(data)
+    })
+}
+
+$(function(){
+    let email=$('#employeeid')
+    let pass=$('#inputPassword')
+    console.log(email.val())
+    $('#btn1').click(()=>{
+        login(email.val(),pass.val(),function(data){
+            console.log(data)
+            console.log(data.token)
+       if(data.token===null) { 
+        let contain=$('#contain')
+       contain.empty()
+       contain.append($(`<p class="text-danger">Incorrect ID or Password</p>`))}
+     else{
+             window.localStorage.token=data.token
+           window.location.replace('https://bugslayers.netlify.app/dashboard')
+        }
+        })
+    })
+})
---
+++ public/signnup.js
@@ -0,0 +1,35 @@
+function signup(name,id,pass,city_val,done){
+    $.post('https://bug-slayers.herokuapp.com/official/signup',{
+        name:name,
+        employee_id:id,
+        password:pass,
+        city:city_val
+    },function(data){
+           done(data)
+    })
+}
+
+$(function(){
+let id=$('#employeeid')
+let name=$('#name')
+let pass=$('#inputPassword')
+let city=$('#city')
+$('#btn1').click(()=>{
+  if(name.val()==='' || pass.val()===''  || id.val()==='' || city.val()===''){
+      window.alert("Please enter all the fields")
+  }
+  else{
+      signup(name.val(),id.val(),pass.val(),city.val(),function(data){
+            console.log(data)
+            if(data.error!=null){ 
+                let contain=$('#contain')
+                contain.empty()
+                contain.append($(`<p class="text-danger">This User Already exists</p>`))
+            }
+            else{
+                window.location.replace('https://bugslayers.netlify.app/public/login')
+            }
+      })
+  }
+})
+})
